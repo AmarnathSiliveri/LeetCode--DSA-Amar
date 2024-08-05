6620b7fce8f8d028cc105eb8aaa3fa28b599cddf
Siliveri Amarnath <154747575+AmarnathSiliveri@users.noreply.github.com>
2024-08-05 20:33:14 +0530

    Time: 7 ms (17.4%), Space: 44.5 MB (5.49%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
-
 class Solution {
-    public boolean isPalindrome(int x) {
+    public boolean isPalindrome(int x) 
+    {
+        String num=Integer.toString(x);
+        StringBuilder num2=new StringBuilder();
+        
+        for (int i=num.length()-1;i>=0;i--)
+        {
+            
+            num2.append(num.charAt(i));
 
-        String num = Integer.toString(x);
-        int left = 0;
-        int right = num.length() - 1;
-
-
-        while (left < right) {
-            if (num.charAt(left) != num.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+        }
+        
+        if(num.equals(num2.toString()))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
         }
 
-        return true; 
     }
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        sc.close();
-
-        Solution solution = new Solution();
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int x=sc.nextInt();
+    
+        Solution solution=new Solution();
         System.out.println(solution.isPalindrome(x));
+
     }
 }
