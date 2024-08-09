77b9c9fbd60dc4ac919d62d0312d6c8e041c67cd
Siliveri Amarnath <154747575+AmarnathSiliveri@users.noreply.github.com>
2024-08-09 07:55:45 +0530

    Time: 354 ms (82.19%), Space: 69.3 MB (15.85%) - LeetHub

--- 3076-get-the-size-of-a-dataframe/3076-get-the-size-of-a-dataframe.py
+++ 3076-get-the-size-of-a-dataframe/3076-get-the-size-of-a-dataframe.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
 def getDataframeSize(players: pd.DataFrame) -> List[int]:
-    df=pd.DataFrame(players)
-    k=[df.shape[0],df.shape[1]]
-    return  k
+    return [players.shape[0],players.shape[1]]
     
